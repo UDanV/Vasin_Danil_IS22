c87ab07e5163bce3ca975f370db3810066f866d9
UDanV <114464286+UDanV@users.noreply.github.com>
2023-11-21 19:02:19 +0300

    updated PZ_5.2.py

--- PZ_5/PZ_5.2.py
+++ PZ_5/PZ_5.2.py
@@ -1,11 +1,23 @@
+# Импортируем библиотеку
 import math
-
+# Создаем функцию TrianglePS
 def TrianglePS(a):
-    perimeter = 3 * a
-    area = (a ** 2) * math.sqrt(3) / 4
-    return perimeter, area
+    try:
+        perimeter = 3 * a
+        area = (a ** 2) * math.sqrt(3) / 4
+        return perimeter, area
+    except ValueError:
+        print("Ошибка: Введите корректное числовое значение.")
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
 
+# Создаем цикл, который 3 раза будет запрашивать у пользователя сторону треугольника
 for i in range(3):
-    side = float(input("Введите сторону треугольника: "))
-    perimeter, area = TrianglePS(side)
-    print(f"Периметр треугольника со стороной {side} равен {perimeter}, а площадь равна {area}")
+    try:
+        side = float(input("Введите сторону треугольника: "))
+        perimeter, area = TrianglePS(side)
+        print(f"Периметр треугольника со стороной {side} равен {perimeter}, а площадь равна {round(area, 2)}")
+    except ValueError:
+        print("Ошибка: Введите корректное числовое значение.")
+    except Exception as error:
+        print(f"Произошла ошибка: {error}")
