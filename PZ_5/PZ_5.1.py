ea7303e97013b2925aea0e0e3df2b91c61423839
UDanV <114464286+UDanV@users.noreply.github.com>
2023-11-21 19:02:47 +0300

    updated PZ_5.1.py

--- PZ_5/PZ_5.1.py
+++ PZ_5/PZ_5.1.py
@@ -1,11 +1,17 @@
+# Создаем функцию sum_user_numbers
 def sum_user_numbers():
-    start = int(input("Введите начало числового ряда: "))
-    end = int(input("Введите конец числового ряда: "))
-    total = 0
-    for i in range(start, end + 1):
-        total += i
-    return total
-
+    try:
+        start = int(input("Введите начало числового ряда: "))
+        end = int(input("Введите конец числового ряда: "))
+        total = 0
+        # Создаем цикл, который будет выводить ответ в зависимости от введенных данных
+        for i in range(start, end + 1):
+            total += i
+        return total
+    except ValueError:
+        print("Ошибка: Введите корректное числовое значение.")
+    except Exception as error:
+        print(f"Произошла ошибка: {error}")
 
 result = sum_user_numbers()
-print(result)  # Выведет сумму числового ряда, введенного пользователем
+print(result) # Выведет сумму числового ряда, введенного пользователем
