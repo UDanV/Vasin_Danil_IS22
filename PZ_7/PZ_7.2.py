2324c48754b62c175de4ddc0eab516061f017a67
UDanV <danil17092006@gmail.com>
2023-12-11 15:04:41 +0300

    modified PZ_7

--- PZ_7/PZ_7.2.py
+++ PZ_7/PZ_7.2.py
@@ -2,17 +2,21 @@
 # упорядочены по алфавиту, то вывести 0; в противном случае вывести номер первого
 # символа строки, нарушающего алфавитный порядок.
 def check_alphabetic_order(s):
-    if not s:
-        return -1
-    for i in range(len(s)-1):
-        if s[i] > s[i+1]:
-            return i+1
-    return 0
+    try:
+        if not s:
+            return -1
+        for i in range(len(s) - 1):
+            if s[i] > s[i + 1]:
+                return i + 1
+        return 0
+    except Exception as error:
+        print(f"Ошибка: {error}")
 
-# Пример использования
-input_str = input("Введите строку: ")
+input_str = input("Введите строку: ")  # Ввод пользователем строки
 result = check_alphabetic_order(input_str)
-if result != 0:
+if result == -1:
+    print("Ошибка. Строка пуста")
+elif result != 0:
     print(f"Номер первого символа, нарушающего алфавитный порядок: {result + 1}")
 else:
     print("Строка упорядочена по алфавиту")
