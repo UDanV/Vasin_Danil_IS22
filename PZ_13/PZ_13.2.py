e63f99b54fda9af8c22d93510fa114117bdb4baf
UDanV <114464286+UDanV@users.noreply.github.com>
2024-02-21 11:06:17 +0300

    Update PZ_13.2.py

--- PZ_13/PZ_13.2.py
+++ PZ_13/PZ_13.2.py
@@ -2,12 +2,13 @@
 Из матрицы сформировать массив из положительных четных элементов, найти их
 сумму и среднее арифметическое.
 """
-matrix = [[1, 2, 3], [4, -5, 6], [7, 8, -9]]
-positive_even = []  # Массив для хранения положительных четных элементов
+matrix = [[i+j for j in range(-1, 4)] for i in range(4)]
+print("Исходная матрица", matrix)
+positive_even = []  # Здесь будем хранить положительные четные элементы
 for row in matrix:
     for element in row:
         if element > 0 and element % 2 == 0:  # Проверка на четность и положительность
-            positive_even.append(element)  # Добавляем элемент в массив
+            positive_even.append(element)  # Аппендим (добавляем) элемент в массив
 print("Массив положительных четных элементов:", positive_even)
 print("Сумма положительных четных элементов:", sum(positive_even))
-print("Среднее арифметическое положительных четных элементов:", sum(positive_even) / len(positive_even))
+print("Среднее арифметическое положительных четных элементов:", round(sum(positive_even) / len(positive_even), 1))
