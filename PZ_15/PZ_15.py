e67c9934eff51db7b9c238fb8d8b3f16680af418
UDanV <114464286+UDanV@users.noreply.github.com>
2024-04-03 11:04:41 +0300

    update PZ_15

--- PZ_15/PZ_15.py
+++ PZ_15/PZ_15.py
@@ -1,34 +1,47 @@
+"""
+Приложение КАФЕДРА для автоматизации работы отдела кадров ВУЗа. Таблица
+Преподавательский состав должна содержать следующие данные: Табельный номер,
+Фамилия И.О., Дата рождения, Должность, Ученая степень, Нагрузка, Зарплата
+"""
+
 import sqlite3
 
 conn = sqlite3.connect('kafedra.db')
 c = conn.cursor()
 
 c.execute('''
-CREATE TABLE IF NOT EXISTS Преподавательский_состав (
-    Табельный_номер INTEGER PRIMARY KEY,
-    Фамилия_ИО TEXT NOT NULL,
-    Дата_рождения DATE NOT NULL,
-    Должность TEXT NOT NULL,
-    Ученая_степень TEXT,
-    Нагрузка REAL NOT NULL,
-    Зарплата REAL NOT NULL
+CREATE TABLE IF NOT EXISTS TeachingStaff (
+    id_teacher INT PRIMARY KEY,
+    fullName VARCHAR NOT NULL,
+    dateBirth DATE NOT NULL,
+    job VARCHAR NOT NULL,
+    academicDegree VARCHAR,
+    load REAL NOT NULL,
+    payment REAL NOT NULL
 )
 ''')
 
 def addTeacher(number, fullname, dateBirth, job, academicDegree, load, payment):
     c.execute('''
-    INSERT INTO Преподавательский_состав (Табельный_номер, Фамилия_ИО, Дата_рождения, Должность, Ученая_степень, Нагрузка, Зарплата)
+    INSERT INTO TeachingStaff (id_teacher, fullName, dateBirth, job, academicDegree, load, payment)
     VALUES (?, ?, ?, ?, ?, ?, ?)
     ''', (number, fullname, dateBirth, job, academicDegree, load, payment))
     conn.commit()
 
-
 addTeacher(1, 'Иванов И.И.', '1980-01-01', 'Профессор', 'Доктор наук', 18, 60000)
+addTeacher(2, 'Драгунов В.В.', '1979-17-07', 'Преподаватель', 'Магистр', 10, 49000)
+addTeacher(3, 'Менделеев Д.И.', '1834-08-02', 'Профессор', 'Доктор наук', 22, 78000)
+
+
+def dropTable():
+    c.execute('DROP TABLE IF EXISTS TeachingStaff')
+    conn.commit()
 
 def getTeacher():
-    c.execute('SELECT * FROM Преподавательский_состав')
+    c.execute('SELECT * FROM TeachingStaff')
     return c.fetchall()
 
 for teacher in getTeacher():
     print(teacher)
+
 conn.close()
