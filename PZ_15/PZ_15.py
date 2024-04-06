c4100ecbdbd6730d1212b84facc826b167fb6c74
UDanV <114464286+UDanV@users.noreply.github.com>
2024-04-06 12:59:50 +0300

    update PZ_15

--- PZ_15/PZ_15.py
+++ PZ_15/PZ_15.py
@@ -3,45 +3,84 @@
 Преподавательский состав должна содержать следующие данные: Табельный номер,
 Фамилия И.О., Дата рождения, Должность, Ученая степень, Нагрузка, Зарплата
 """
+import sqlite3 as sq
 
-import sqlite3
+with sq.connect('kafedra.db') as c:
+    con = c.cursor()
+    con.execute("DROP TABLE IF EXISTS teachingStaff")
+    con.execute("""CREATE TABLE IF NOT EXISTS teachingStaff (
+                id_teacher INTEGER PRIMARY KEY AUTOINCREMENT,
+                fullName VARCHAR,
+                dateBirth DATE,
+                job VARCHAR,
+                academicDegree VARCHAR,
+                load INTEGER,
+                payment REAL
+                )""")
+    c.commit()
+    con.close()
 
-conn = sqlite3.connect('kafedra.db')
-c = conn.cursor()
-
-c.execute('''
-CREATE TABLE IF NOT EXISTS TeachingStaff (
-    id_teacher INT PRIMARY KEY,
-    fullName VARCHAR NOT NULL,
-    dateBirth DATE NOT NULL,
-    job VARCHAR NOT NULL,
-    academicDegree VARCHAR,
-    load REAL NOT NULL,
-    payment REAL NOT NULL
-)
-''')
-
-def addTeacher(number, fullname, dateBirth, job, academicDegree, load, payment):
-    c.execute('''
+def addTeacher(number, fullname, dateBirth, job, academicDegree, load, payment): # Добавляем элементы со значениями ?
+    con = c.cursor()
+    con.execute('''
     INSERT INTO TeachingStaff (id_teacher, fullName, dateBirth, job, academicDegree, load, payment)
     VALUES (?, ?, ?, ?, ?, ?, ?)
     ''', (number, fullname, dateBirth, job, academicDegree, load, payment))
-    conn.commit()
+    c.commit()
 
 addTeacher(1, 'Иванов И.И.', '1980-01-01', 'Профессор', 'Доктор наук', 18, 60000)
 addTeacher(2, 'Драгунов В.В.', '1979-17-07', 'Преподаватель', 'Магистр', 10, 49000)
 addTeacher(3, 'Менделеев Д.И.', '1834-08-02', 'Профессор', 'Доктор наук', 22, 78000)
+addTeacher(4, 'Шелбузов А.И.', '1974-21-08', 'Доцент', 'Бакалавр', 14, 52500)
+addTeacher(5, 'Аванасов Ю.А.', '1945-15-04', 'Преподаватель', 'Магистр', 17, 58900)
+addTeacher(6, 'Шестаков Д.И.', '1927-30-01', 'Преподаватель', 'Бакалавр', 20, 70000)
+addTeacher(7, 'Гайдай В.Д.', '1987-08-02', 'Профессор', 'Доктор наук', 13, 51700)
+addTeacher(8, 'Макаридзе Д.О.', '1977-12-09', 'Доцент', 'Бакалавр', 14, 52500)
+addTeacher(9, 'Булыгин В.В.', '1999-01-01', 'Профессор', 'Доктор наук', 18, 60000)
+addTeacher(10, 'Путин А.В.', '1941-02-02', 'Преподаватель', 'Магистр', 10, 49000)
 
+def findByPosition(job):
+    """Поиск по должности"""
+    con = sq.connect("kafedra.db")
+    c = con.cursor()
+    c.execute('''SELECT * FROM teachingStaff WHERE job=?''', (job,))
+    result = c.fetchall()
+    con.close()
+    return result
 
-def dropTable():
-    c.execute('DROP TABLE IF EXISTS TeachingStaff')
-    conn.commit()
+def findByDegree(academicDegree):
+    """Поиск по ученой степени"""
+    con = sq.connect("kafedra.db")
+    c = con.cursor()
+    c.execute('''SELECT * FROM teachingStaff WHERE academicDegree=?''', (academicDegree,))
+    result = c.fetchall()
+    con.close()
+    return result
+
+def findByPayment(payment):
+    """Поиск по зарплате"""
+    con = sq.connect("kafedra.db")
+    c = con.cursor()
+    c.execute('''SELECT * FROM teachingStaff WHERE payment=?''', (payment,))
+    result = c.fetchall()
+    con.close()
+    return result
+
+def deleteByNumber(teacher_id):
+    """Удаление по номеру"""
+    con = sq.connect("kafedra.db")
+    c = con.cursor()
+    c.execute('''DELETE FROM teachingStaff WHERE teacher_id=?''', (teacher_id, ))
+    con.commit()
+    con.close()
 
 def getTeacher():
+    con = sq.connect("kafedra.db")
+    c = con.cursor()
     c.execute('SELECT * FROM TeachingStaff')
     return c.fetchall()
 
 for teacher in getTeacher():
     print(teacher)
 
-conn.close()
+con.close()
